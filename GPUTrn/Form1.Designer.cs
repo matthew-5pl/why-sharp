74845562cbec873482b99e4fa8ea2367c807fcf6
Matthew <43993777+matthew-5pl@users.noreply.github.com>
2020-11-30 12:42:37 +0100

    i was bored so i overlapped some triangles in csharp and i made a loop preference.

---
+++ GPUTrn/Form1.Designer.cs
@@ -0,0 +1,96 @@
+﻿namespace GPUTrn
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variabile di progettazione necessaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Pulire le risorse in uso.
+        /// </summary>
+        /// <param name="disposing">ha valore true se le risorse gestite devono essere eliminate, false in caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Codice generato da Progettazione Windows Form
+
+        /// <summary>
+        /// Metodo necessario per il supporto della finestra di progettazione. Non modificare
+        /// il contenuto del metodo con l'editor di codice.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.panel3 = new System.Windows.Forms.Panel();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.Red;
+            this.panel1.Location = new System.Drawing.Point(137, 161);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(273, 171);
+            this.panel1.TabIndex = 0;
+            // 
+            // panel2
+            // 
+            this.panel2.BackColor = System.Drawing.Color.Lime;
+            this.panel2.Location = new System.Drawing.Point(438, 149);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(258, 136);
+            this.panel2.TabIndex = 1;
+            // 
+            // panel3
+            // 
+            this.panel3.BackColor = System.Drawing.Color.Blue;
+            this.panel3.Location = new System.Drawing.Point(295, 12);
+            this.panel3.Name = "panel3";
+            this.panel3.Size = new System.Drawing.Size(134, 113);
+            this.panel3.TabIndex = 2;
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Location = new System.Drawing.Point(12, 12);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(89, 17);
+            this.checkBox1.TabIndex = 3;
+            this.checkBox1.Text = "isTestRepeat";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            this.checkBox1.CheckedChanged += new System.EventHandler(this.checkBox1_CheckedChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.checkBox1);
+            this.Controls.Add(this.panel3);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.panel1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Panel panel3;
+        private System.Windows.Forms.CheckBox checkBox1;
+    }
+}
+
---
+++ GPUTrn/Form1.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace GPUTrn
+{
+    public partial class Form1 : Form
+    {
+        int pos1 = 1;
+        int pos2 = -1;
+        Timer timer = new Timer();
+        Timer timer2 = new Timer();
+        public Form1()
+        {
+            InitializeComponent();
+            timer2.Interval = 6000;
+            timer2.Tick += new EventHandler(timer2_Tick);
+            if (checkBox1.Checked == true)
+            {
+                timer2.Start();
+            }
+            timer.Interval = 1;
+            timer.Tick += new EventHandler(timer_Tick);
+            timer.Start();
+            if (Properties.Settings.Default.isChecked == true)
+            {
+                checkBox1.Checked = true;
+            }
+
+            else if (Properties.Settings.Default.isChecked == false)
+            {
+                checkBox1.Checked = false;
+            }
+        }
+
+        void timer_Tick (object sender, EventArgs e)
+        {
+            panel1.Location = new Point(panel1.Location.X + pos1, panel1.Location.Y + pos1);
+            panel2.Location = new Point(panel2.Location.X + pos2, panel2.Location.Y + pos2);
+            panel3.Location = new Point(panel3.Location.X, panel3.Location.Y + pos1);
+        }
+
+        void timer2_Tick (object sender, EventArgs e)
+        {
+            panel1.Location = new Point(273, 171);
+            panel2.Location = new Point(438, 149);
+            panel3.Location = new Point(295, 12);
+            Console.Beep();
+        }
+
+        private void checkBox1_CheckedChanged(object sender, EventArgs e)
+        {
+            if (checkBox1.Checked == true)
+            {
+                timer2.Start();
+                Properties.Settings.Default.isChecked = true;
+                Properties.Settings.Default.Save();
+            }
+
+            else if (checkBox1.Checked == false)
+            {
+                timer2.Stop();
+                Properties.Settings.Default.isChecked = false;
+                Properties.Settings.Default.Save();
+            }
+        }
+    }
+}
